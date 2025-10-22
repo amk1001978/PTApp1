5264f060852fcac05019f09eb95cf10f9bab24c4
MD Sabir Hossin <amk1001978@student.hamk.fi>
2025-10-22 11:31:38 +0300

    Initial commit

---
+++ src/PrinterApp.java
@@ -0,0 +1,30 @@
+public class PrinterApp {
+    public static void main(String[] args) {
+       
+        String creator = "MD Sabir Hossin";
+        int number1 = 5;
+        int number2 = 2;
+        int multiplication;
+        int addition;
+        int subtraction;
+        double division;
+
+        System.out.println("Printer App");
+        System.out.print("This app is made by: ");
+        System.out.println(creator);
+
+        
+        System.out.println("The value of number1 is " + number1);
+        System.out.println("The value of number2 is " + number2);
+
+        multiplication = number1 * number2;
+        addition = number1 + number2;
+        subtraction = number1 - number2;
+        division = (double) number1 / number2;
+
+        System.out.println(number1 + " * " + number2 + " = " + multiplication);
+        System.out.println(number1 + " + " + number2 + " = " + addition);
+        System.out.println(number1 + " - " + number2 + " = " + subtraction);
+        System.out.println(number1 + " / " + number2 + " = " + division);
+    }
+}
